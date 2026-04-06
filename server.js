4e1dcab4b2485ec75acd6b6053360ba2597b34f2
Aditya <singh010aditya@gmail.com>
2026-04-06 22:03:18 +0530

    Test auth route

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(cors());
 app.use(express.json());
 
 // ALL ROUTES COMMENTED
-// app.use("/api/auth", require("./routes/authRoutes"));
+app.use("/api/auth", require("./routes/authRoutes"));
 // app.use("/api/trips", require("./routes/tripRoutes"));
 // app.use("/api/bookings", require("./routes/bookingRoutes"));
 // app.use("/api/payment", require("./routes/paymentRoutes"));
